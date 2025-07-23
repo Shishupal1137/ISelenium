ad807828f3a17959ecbea08b45e864522cff61f6
Shishupal <shishupalsingh1137@gmail.com>
2025-07-23 19:14:27 +0530

    Democlass

--- src/test/java/testdata.java
+++ src/test/java/testdata.java
@@ -1,27 +1,3 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 public class testdata {
-
-    public static void main(String[] args) {
-
-        // Directly launch ChromeDriver (assuming chromedriver is in PATH)
-        WebDriver driver = new ChromeDriver();
-
-        // Open Flipkart
-        driver.get("https://www.flipkart.com");
-
-        // Print the title of the Flipkart page
-        System.out.println("Page title is: " + driver.getTitle());
-
-        // Wait for a few seconds to see the page (optional)
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Close the browser
-        driver.quit();
-    }
+    
 }
